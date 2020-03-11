c2463cc8612a3dbeed2667f7064cd5824e5ff9d9
hayamizhin <max9173627>
2020-03-11 13:37:03 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :high_scores
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'high_scores#index'
+end
